32c19eca4e4ad6753a4b76d2024659b7b323443f
RakeshYadavGit <rryadav142001@gmail.com>
2023-02-05 23:31:54 +0530

    3nd Commit
    1. Implemented Back-End logic of MainActivity.kt
    2. created four kt and xml files and connected with MainActivity.kt

--- app/src/main/java/com/example/componentsofandroid/MainActivity.kt
+++ app/src/main/java/com/example/componentsofandroid/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.componentsofandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun goToActivity(view: View) {
+        startActivity(Intent(this, ActivityLifeCycle::class.java))
+    }
+    fun goToService(view: View) {
+        startActivity(Intent(this, ServiceActivity::class.java))
+    }
+    fun goToBroadcastReceiver(view: View) {
+        startActivity(Intent(this,BroadcastReceiverActivity::class.java))
+    }
+    fun goToContentProvider(view: View) {
+        startActivity(Intent(this, ContentProviderActivity::class.java))
     }
 }
