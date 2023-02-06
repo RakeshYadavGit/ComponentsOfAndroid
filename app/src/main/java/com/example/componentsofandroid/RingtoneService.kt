1cb4133c6ebaced569ad4de737e14d8915b43368
RakeshYadavGit <rryadav142001@gmail.com>
2023-02-06 08:16:37 +0530

    7th Commit Service 1. RingtoneService.kt logic implemented 2. ServiceActivity.kt logic implemented

---
+++ app/src/main/java/com/example/componentsofandroid/RingtoneService.kt
@@ -0,0 +1,27 @@
+package com.example.componentsofandroid
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+import android.provider.Settings
+
+class RingtoneService : Service() {
+    private lateinit var player: MediaPlayer
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        player = MediaPlayer.create(this, Settings.System.DEFAULT_ALARM_ALERT_URI)
+        player.isLooping = true
+        player.start()
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player.stop()
+    }
+
+}
--- app/src/main/java/com/example/componentsofandroid/ServiceActivity.kt
+++ app/src/main/java/com/example/componentsofandroid/ServiceActivity.kt
@@ -1,11 +1,21 @@
 package com.example.componentsofandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ServiceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_service)
+    }
+
+    fun startRingtoneService(view: View) {
+        startService(Intent(this,RingtoneService::class.java))
+    }
+
+    fun stopRingtoneService(view: View) {
+        stopService(Intent(this,RingtoneService::class.java))
     }
 }
