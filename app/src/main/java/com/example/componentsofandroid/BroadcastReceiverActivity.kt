133584d13bc68a3b4c411c1289b6e65a203e9ad0
RakeshYadavGit <rryadav142001@gmail.com>
2023-02-06 11:30:09 +0530

    8th Commit Broadcast and Content 1. BroadcastReceiverActivity.kt logic implemented 2. ContentProviderActivity.kt logic implemented

--- app/src/main/java/com/example/componentsofandroid/BroadcastReceiverActivity.kt
+++ app/src/main/java/com/example/componentsofandroid/BroadcastReceiverActivity.kt
@@ -1,11 +1,33 @@
 package com.example.componentsofandroid
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.core.app.ActivityCompat
 
 class BroadcastReceiverActivity : AppCompatActivity() {
+
+    val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+    lateinit var receiver: AirplaneModeChangeReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_broadcast_receiver)
+        receiver = AirplaneModeChangeReceiver()
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            registerReceiver(receiver, it)
+        }
     }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
+        }
+
+
 }
--- app/src/main/java/com/example/componentsofandroid/ContentProviderActivity.kt
+++ app/src/main/java/com/example/componentsofandroid/ContentProviderActivity.kt
@@ -1,11 +1,36 @@
 package com.example.componentsofandroid
 
+import android.content.pm.PackageManager
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.View
+import android.widget.ListView
+import android.widget.SimpleAdapter
+import android.widget.SimpleCursorAdapter
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
 class ContentProviderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content_provider)
+
+        read()
     }
+    fun read(){
+        val resultView = findViewById<ListView>(R.id.list)
+        val cursor : Cursor? = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null)
+        startManagingCursor(cursor)
+        val form = arrayOf(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER,ContactsContract.CommonDataKinds.Phone._ID)
+        val listData = intArrayOf(android.R.id.text1,android.R.id.text2)
+        val simple : SimpleCursorAdapter = SimpleCursorAdapter(this,android.R.layout.simple_list_item_2,cursor,form,listData)
+        resultView.adapter=simple
+    }
+
+
+
+
+
 }
